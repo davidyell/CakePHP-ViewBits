69a5eb4d876a74c157268aeb6732bce8beae48d8
David Yell <dyell@ukwebmedia.com>
2014-05-15 10:23:31 +0100

    Resolves #3. Created a validation method to ensure a valid route

--- Controller/ViewBitsController.php
+++ Controller/ViewBitsController.php
@@ -17,46 +17,46 @@ class ViewBitsController extends ViewBitsAppController {
 /**
  * Creates a list of viewbits on the system
  */
-    public function admin_index(){
-        $this->set('viewBits', $this->Paginator->paginate());
-    }
+	public function admin_index() {
+		$this->set('viewBits', $this->Paginator->paginate());
+	}
 
 /**
  * Create a new viewbit
  */
-    public function admin_add(){
-        if($this->request->is('post') || $this->request->is('put')){
-            $this->ViewBit->create();
-            if($this->ViewBit->save($this->request->data)){
-                $this->Session->setFlash(__('The view bit has been saved'), 'NiceAdmin.alert-box', array('class'=>'alert-success'));
-                $this->redirect(array('action'=>'index'));
-            } else{
-                $this->Session->setFlash(__('The view bit could not be saved. Please, try again.'), 'alert-box', array('class'=>'alert-error'));
-            }
-        }
-    }
+	public function admin_add() {
+		if ($this->request->is('post') || $this->request->is('put')) {
+			$this->ViewBit->create();
+			if ($this->ViewBit->save($this->request->data)) {
+				$this->Session->setFlash(__('The view bit has been saved'), null, array('class' => 'alert-success'));
+				$this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash(__('The view bit could not be saved. Please, try again.'), null, array('class' => 'alert-error'));
+			}
+		}
+	}
 
 /**
  * Edit and existing viewbit
  * @param int $id
  * @throws NotFoundException
  */
-    public function admin_edit($id = null){
-        $this->ViewBit->id = $id;
-        if(!$this->ViewBit->exists()){
-            throw new NotFoundException(__('Invalid content'));
-        }
-        if($this->request->is('post') || $this->request->is('put')){
-            if($this->ViewBit->save($this->request->data)){
-                $this->Session->setFlash(__('The view bit has been saved'), 'NiceAdmin.alert-box', array('class'=>'alert-success'));
-                $this->redirect(array('action'=>'index'));
-            } else{
-                $this->Session->setFlash(__('The view bit could not be saved. Please, try again.'), 'NiceAdmin.alert-box', array('class'=>'alert-error'));
-            }
-        } else{
-            $this->request->data = $this->ViewBit->read(null, $id);
-        }
-    }
+	public function admin_edit($id = null) {
+		$this->ViewBit->id = $id;
+		if (!$this->ViewBit->exists()) {
+			throw new NotFoundException(__('Invalid content'));
+		}
+		if ($this->request->is('post') || $this->request->is('put')) {
+			if ($this->ViewBit->save($this->request->data)) {
+				$this->Session->setFlash(__('The view bit has been saved'), null, array('class' => 'alert-success'));
+				$this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash(__('The view bit could not be saved. Please, try again.'), null, array('class' => 'alert-error'));
+			}
+		} else {
+			$this->request->data = $this->ViewBit->read(null, $id);
+		}
+	}
 
 /**
  * Delete a viewbit
@@ -64,20 +64,20 @@ class ViewBitsController extends ViewBitsAppController {
  * @throws MethodNotAllowedException
  * @throws NotFoundException
  */
-    public function admin_delete($id = null){
-        if(!$this->request->is('post')){
-            throw new MethodNotAllowedException();
-        }
-        $this->ViewBit->id = $id;
-        if(!$this->ViewBit->exists()){
-            throw new NotFoundException(__('Invalid content'));
-        }
-        if($this->ViewBit->delete()){
-            $this->Session->setFlash(__('ViewBit deleted'), 'NiceAdmin.alert-box', array('class'=>'alert-success'));
-            $this->redirect(array('action'=>'index'));
-        }
-        $this->Session->setFlash(__('ViewBit was not deleted'), 'NiceAdmin.alert-box', array('class'=>'alert-error'));
-        $this->redirect(array('action'=>'index'));
-    }
+	public function admin_delete($id = null) {
+		if (!$this->request->is('post')) {
+			throw new MethodNotAllowedException();
+		}
+		$this->ViewBit->id = $id;
+		if (!$this->ViewBit->exists()) {
+			throw new NotFoundException(__('Invalid content'));
+		}
+		if ($this->ViewBit->delete()) {
+			$this->Session->setFlash(__('ViewBit deleted'), 'NiceAdmin.alert-box', array('class' => 'alert-success'));
+			$this->redirect(array('action' => 'index'));
+		}
+		$this->Session->setFlash(__('ViewBit was not deleted'), 'NiceAdmin.alert-box', array('class' => 'alert-error'));
+		$this->redirect(array('action' => 'index'));
+	}
 
 }
--- Model/ViewBit.php
+++ Model/ViewBit.php
@@ -1,5 +1,8 @@
 <?php
+
 App::uses('ViewBitsAppModel', 'ViewBits.Model');
+App::uses('HttpSocket', 'Network/Http');
+
 /**
  * ViewBit Model
  *
@@ -18,28 +21,48 @@ class ViewBit extends ViewBitsAppModel {
  * 
  * @var array
  */
-        public $validate = array(
-            'name'=>array(
-                'one'=>array(
-                    'rule'=>'notEmpty',
-                    'message'=>'Please enter a name for this item',
-                    'required'=>true
-                )
-            ),
-            'route'=>array(
-                'one'=>array(
-                    'rule'=>'notEmpty',
-                    'message'=>'You must enter a route',
-                    'required'=>true
-                )
-            ),
-            'content'=>array(
-                'one'=>array(
-                    'rule'=>'notEmpty',
-                    'message'=>'Please enter some content to display',
-                    'required'=>true
-                )
-            )
-        );
+	public $validate = [
+		'name' => [
+			'one' => [
+				'rule' => 'notEmpty',
+				'message' => 'Please enter a name for this item',
+				'required' => true
+			]
+		],
+		'route' => [
+			'one' => [
+				'rule' => 'notEmpty',
+				'message' => 'You must enter a route',
+				'required' => true
+			],
+			'two' => [
+				'rule' => 'check_route',
+				'message' => 'Route is not a valid page'
+			]
+		],
+		'content' => [
+			'one' => [
+				'rule' => 'notEmpty',
+				'message' => 'Please enter some content to display',
+				'required' => true
+			]
+		]
+	];
 
+/**
+ * Check to make sure that the route is reachable and will return a 200 OK
+ * 
+ * @param array $check
+ * @return boolean
+ */
+	public function check_route($check) {
+		$http = new HttpSocket();
+		$response = $http->get(Router::fullBaseUrl() . $check['route']);
+		
+		if ((int)$response->code === 200) {
+			return true;
+		}
+		
+		return false;
+	}
 }
