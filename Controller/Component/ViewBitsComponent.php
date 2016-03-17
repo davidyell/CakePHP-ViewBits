0a806b67644ae1659c166d78dfbce290f4c733eb
David Yell <dyell@ukwebmedia.com>
2016-03-17 16:13:52 +0000

    Cache the data, and filter for static file requests

--- Controller/Component/ViewBitsComponent.php
+++ Controller/Component/ViewBitsComponent.php
@@ -31,32 +31,35 @@ class ViewBitsComponent extends Component {
  */
     public function beforeRender(Controller $controller) {
         parent::beforeRender($controller);
-        
-        // Check to make sure it's not ajax/json/xml
-        if($controller->request->is('get') || $controller->request->is('post') || $controller->request->is('put')){
-            
-            $controller->loadModel('ViewBits.ViewBit');
 
-            $options = [
-                'conditions' => [
-                    'OR' => [
-                        ['route' => $controller->here],
-                        ['route' => '*']
-                    ]
-                ],
-                'order' => ['order ASC']
-            ];
+        if(!$controller->request->is('ajax') && preg_match("/^\/(files|css|img|js)/", $controller->here) === 0){
 
-            $options['conditions'] = array_merge($options['conditions'], $this->settings['conditions']);
+            $key = 'viewbit_' . Inflector::slug($controller->here);
+            $bits = Cache::read($key, 'medium');
 
-            $bits = $controller->ViewBit->find('all', $options);
-            
+            if (!$bits) {
+                $controller->loadModel('ViewBits.ViewBit');
+
+                $options = [
+                    'conditions' => [
+                        'OR' => [
+                            ['route' => $controller->here],
+                            ['route' => '*']
+                        ]
+                    ],
+                    'order' => ['order ASC']
+                ];
+
+                $options['conditions'] = array_merge($options['conditions'], $this->settings['conditions']);
+
+                $bits = $controller->ViewBit->find('all', $options);
+
+                Cache::write($key, $bits, 'medium');
+            }
+
             // Set the data in the view for the helper to use
             $controller->set('viewbits', $bits);
         }
-        
     }
     
 }
-
-?>
